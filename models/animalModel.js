5a8dcd74bf06c74ea17557ca0e797d4f0ec4acfb
Luis Bovo <dark.luismtns@gmail.com>
2024-05-27 16:57:24 -0300

    insert animal model field

--- models/animalModel.js
+++ models/animalModel.js
@@ -2,9 +2,10 @@ class Animal {
   constructor(id, name, description, dreams, number) {
     (this.id = id),
       (this.name = name),
+      (this.number = number),
       (this.description = description),
       (this.dreams = dreams),
-      (this.number = number);
+      (this.imagesUrls = imagesUrls);
   }
 }
 
