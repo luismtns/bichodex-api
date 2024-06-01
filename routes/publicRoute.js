761d817d1976778f7e9de7603deced3503f98e04
Luis Bovo <dark.luismtns@gmail.com>
2024-06-01 12:58:29 -0300

    v1.0

--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -1,13 +1,6 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
-
-// // Middleware para servir arquivos estáticos
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// app.use(express.static(path.join(__dirname, 'public')));
 
 const router = express.Router();
 
